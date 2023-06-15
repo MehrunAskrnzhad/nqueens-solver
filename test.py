2267fc1f418e801c35efee3fa9056bb4f58b5820
Mehran Askarnezhad <78827397+MehrunAskrnzhad@users.noreply.github.com>
2023-06-15 19:28:49 +0330

    fixed for pylint

--- test.py
+++ test.py
@@ -1,20 +1,21 @@
-from nqueenssolver import NQueensSolver
 import unittest
+from nqueenssolver import NQueensSolver
+
 
 class NQueensSolverTestCase(unittest.TestCase):
-    def testSolutionExists(self):
+    def test_solution_exists(self):
         """
         Test case to verify that a solution exists for a valid board size.
         """
         solver = NQueensSolver(4)
         self.assertIsNotNone(solver.solution)
 
-    def testGetChessboard(self):
+    def test_get_chessboard(self):
         """
         Test case to verify the correctness of the chessboard representation.
         """
         solver = NQueensSolver(4)
-        expectedChessboard = (
+        expected_chessboard = (
             "+---+---+---+---+\n"
             "|   | Q |   |   |\n"
             "+---+---+---+---+\n"
@@ -25,20 +26,20 @@ class NQueensSolverTestCase(unittest.TestCase):
             "|   |   | Q |   |\n"
             "+---+---+---+---+\n"
         )
-        self.assertEqual(solver.getChessboard(), expectedChessboard)
+        self.assertEqual(solver.get_chessboard(), expected_chessboard)
 
-    def testGetSolutionMatrix(self):
+    def test_get_solution_matrix(self):
         """
         Test case to verify the correctness of the solution matrix representation.
         """
         solver = NQueensSolver(4)
-        expectedMatrix = [
+        expected_matrix = [
             [0, 1, 0, 0],
             [0, 0, 0, 1],
             [1, 0, 0, 0],
             [0, 0, 1, 0],
         ]
-        self.assertEqual(solver.getSolutionMatrix(), expectedMatrix)
+        self.assertEqual(solver.get_solution_matrix(), expected_matrix)
 
 
 if __name__ == "__main__":
